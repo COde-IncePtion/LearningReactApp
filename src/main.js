e94ee43476ff9f85b521d0b697c013d25696a663
ashishr350@gmail.com <admin@in-rashish-3.local>
2019-01-13 00:41:06 +0530

    Ashish | adding react-router for routing

--- src/main.js
+++ src/main.js
@@ -1,37 +1,8 @@
-$ = jquery = require('jquery');
 var React = require('react');
-var HomePage = require('./components/homePage');
-var About = require('./components/about/about');
-var Authors = require('./components/authors/authorPage');
-var Header = require('./components/common/header');
+var Router = require('react-router');
+var routes = require('./routes');
 
-var App = React.createClass({
-    render: function () {
-        var Child;
-        switch (this.props.route) {
-            case 'about' :
-                Child = About;
-                break;
-            case 'authors' :
-                Child = Authors;
-                break;
-            default :
-                Child = HomePage;
-        }
 
-        return (
-            <div>
-                <Header/>
-                <Child/>
-            </div>
-        )
-    }
-});
-
-function render() {
-    var route = window.location.hash.substr(1);
-    React.render(<App route={route}/>, document.getElementById('app'));
-}
-
-window.addEventListener('hashchange', render);
-render();
+Router.run(routes, function (Handler) {
+    React.render(<Handler/>, document.getElementById("app"));
+})
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+
+var React = require('react');
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+
+
+var routes = (
+    <Route name='app' path='/' handler={require('./components/app')} >
+        <DefaultRoute handler={require('./components/homePage')} />
+    <Route name='authors' handler={require('./components/authors/authorPage')} />
+    <Route name='about'  handler={require('./components/about/about')} />
+    </Route>
+);
+
+module.exports = routes;
+
+
