e3e6756b1d563ac5ea38760a67bdcc92417c0bc7
ashishr350@gmail.com <admin@in-rashish-3.local>
2019-01-13 12:08:38 +0530

    Ashish | converting anchor tags in header to Link provided by react-router

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,14 +1,15 @@
 "use strict";
 var React = require("react");
+var Link = require('react-router').Link;
 
 var Header = React.createClass({
     render: function () {
         return (
             <div>
                 <ul className="nav nav-pills">
-                    <li role="presentation"><a href="#">Home</a></li>
-                    <li role="presentation"><a href="#about">About</a></li>
-                    <li role="presentation"><a href="#authors">Authors</a></li>
+                    <li role="presentation"><Link to="app">Home</Link></li>
+                    <li role="presentation"><Link to="about">About</Link></li>
+                    <li role="presentation"><Link to="authors">Authors</Link></li>
                 </ul>
             </div>
         );
