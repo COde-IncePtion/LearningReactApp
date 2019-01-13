2b8e95cd64ef8a8bda7bd9771ed033c0c4615007
ashishr350@gmail.com <admin@in-rashish-3.local>
2019-01-13 12:55:17 +0530

    Ashish | adding transition check on about page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -2,6 +2,22 @@
 var React = require("react");
 
 var About = React.createClass({
+    
+    statics : {
+        willTransitionTo : function (transition, params, query, callback) {
+            if (!confirm('Are you sure you want to navigate')) {
+                transition.abort();
+            }
+            else
+                callback();
+        },
+        willTransitionFrom : function (transition, component) {
+            if (!confirm('Are you sure you want leave')) {
+                transition.abort();
+            }
+        }
+    },
+    
     render: function () {
         return (
             <div>
