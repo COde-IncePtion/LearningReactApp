ae10b3478d02143a82d36f872caa0bfb84c4c8d4
Ashish Rawat <ashishr350@gmail.com>
2019-02-12 09:01:20 +0530

    Ashish | adding author store to the app

---
+++ src/stores/authorStore.js
@@ -0,0 +1,20 @@
+var Dispatcher = require('../Dispatcher/appDispatcher');
+var ActionTypes = require('../constants/actionTypes');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var CHANGE_EVENT = 'change';
+
+
+var AuthorStore = assign({}, EventEmitter.prototype, {
+    addChangeListener: function (callback) {
+        this.on(CHANGE_EVENT, callback);
+    },
+
+    removeChangeListener: function (callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    },
+
+    emitChange: function () {
+        this.emit(CHANGE_EVENT);
+    }
+});
